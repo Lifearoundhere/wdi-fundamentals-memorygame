67402c2c72f1afad82be4081fc4585e6f7863ba8
Lifearoundhere <lifearoundpss@gmail.com>
2019-05-19 21:00:24 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,36 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay =[];
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
 var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+
 var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo)
+ checkForMatch();
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
+/*
 if (cardsInPlay.length === 2) {
-alert("User flipped " + cardsInPlay);
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!")
-}else{
-	alert("Sorry, try again.")
+	//alert("User flipped " + cardsInPlay);
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+		}else{
+		alert("Sorry, try again.")
+	}
 }
+*/
+console.log("User flipped " + cards[cardId])
+cardsInPlay.push(cards[cardId]);
 }
-console.log("User flipped " + cardsInPlay)
+flipCard(0);
+flipCard(2);
+//Console.log("User flipped " + cardsInPlay)
